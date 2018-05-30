0ecc591ad2553762df641af7e4d0f569dd7ca8b4
nikkishida <39140453+nikkishida@users.noreply.github.com>
2018-05-30 19:03:14 -0400

    Add files via upload

---
+++ My-Online-Library/inc/data.php
@@ -0,0 +1,189 @@
+<?php
+
+$catalog = [];
+//Books
+$catalog[101] = [
+"title" => "the life-changing magic of tidying up",
+"img" => "img/media/KOnmari.jpg",
+  "genre" => "Self-help",
+  "format" => "Paperback",
+  "year" => 2014,
+  "category" => "Books",
+  "authors" => [
+    "marie kondo",
+
+  ],
+  "publisher" => "Ten Speed Press",
+  "isbn" => '9781607747307'
+];
+
+$catalog[102] = [
+"title" => "Sherlock Holmes",
+"img" => "img/media/Sherlockholmes.jpg",
+  "genre" => "Mystery",
+  "format" => "Paperback",
+  "year" => 2002,
+  "category" => "Books",
+  "authors" => [
+    "Sir Arthur Conan Doyle",
+
+  ],
+  "publisher" => "Penguin Publishing Group",
+  "isbn" => '978-0311184045'
+];
+
+$catalog[103] = [
+"title" => "Ready Player One",
+"img" => "img/media/readyplayerone.jpg",
+  "genre" => "Sci-Fi",
+  "format" => "Paperback",
+  "year" => 2012,
+  "category" => "Books",
+  "authors" => [
+    "Ernest Cline",
+
+  ],
+  "publisher" => "Random House",
+  "isbn" => '978-0307887436'
+];
+
+$catalog[104] = [
+"title" => "Harry Potter",
+"img" => "img/media/harrypotter.jpg",
+  "genre" => "Fantasy",
+  "format" => "Paperback",
+  "year" => 2010,
+  "category" => "Books",
+  "authors" => [
+    "J.K. Rowling",
+
+  ],
+  "publisher" => "Scholastic",
+  "isbn" => '0-545-01022-5'
+];
+
+
+//Movies
+$catalog[201] = [
+  "title" => "Coco",
+  "img" => "img/media/Coco.jpg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2017,
+  "category" => "Movies",
+  "director" => "Lee Unkrich",
+  "writers" => [
+    "Adrian Molina and Matthew Aldrich"
+
+  ],
+  "stars" => [
+    "Anthony Gonzales",
+    "Gael Garcia Bernal",
+    "Benjamin Bratt"
+  ]
+];
+
+$catalog[202] = [
+  "title" => "Atomic Blonde",
+  "img" => "img/media/Atomic-Blonde-MD-Posters.jpg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2017,
+  "category" => "Movies",
+  "director" => "David Leitch",
+  "writers" => [
+    "Kurt Johnstand"
+
+  ],
+  "stars" => [
+    "Charlize Theron",
+    "James McAvoy",
+  ]
+];
+
+$catalog[203] = [
+  "title" => "Spirited Away",
+  "img" => "img/media/SpiritedAway.jpg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2017,
+  "category" => "Movies",
+  "director" => "Hayao Miyazaki",
+  "writers" => [
+    "Hayao Miyazaki"
+
+  ],
+  "stars" => [
+    "Rumi Hiiragi",
+    "Miyu Irino",
+    "Mari Natsuki"
+  ]
+];
+
+$catalog[204] = [
+  "title" => "Deadpool",
+  "img" => "img/media/deadpool.jpg",
+  "genre" => "Action,Adventure",
+  "format" => "DVD",
+  "year" => 2017,
+  "category" => "Movies",
+  "director" => "Tim Miller",
+  "writers" => [
+    "Rhett Reese"
+
+  ],
+  "stars" => [
+    "Ryan Reynolds",
+    "Morena Baccarin",
+    "T.J. Miller"
+  ]
+];
+
+
+
+//Music
+$catalog[301] = [
+  "title" => "My Chemical Romance",
+  "img" => "img/media/MyChemicalRomance.jpg",
+  "genre" => "alternative rock",
+  "format" => "CD",
+  "year" => 2001-2013,
+  "category" => "Music",
+  "artist" => "Gerard Arthur Way"
+]; 
+
+$catalog[302] = [
+  "title" => "Adele",
+  "img" => "img/media/adele.jpg",
+  "genre" => "pop, soul",
+  "format" => "CD",
+  "year" => 2006,
+  "category" => "Music",
+  "artist" => "Adele"
+]; 
+
+$catalog[303] = [
+  "title" => "One Ok Rock",
+  "img" => "img/media/OneOkRock.jpg",
+  "genre" => "j-rock",
+  "format" => "CD",
+  "year" => 2005,
+  "category" => "Music",
+  "artist" => "Takahiro Moriuchi"
+]; 
+
+$catalog[304] = [
+  "title" => "Beach House",
+  "img" => "img/media/beachhouse.jpg",
+  "genre" => "dream pop, indie rock",
+  "format" => "CD",
+  "year" => 2004,
+  "category" => "Music",
+  "artist" => "Victoria Legrand"
+]; 
+
+
+
+
+
+
